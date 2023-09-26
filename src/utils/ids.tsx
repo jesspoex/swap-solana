bcf4fe554188eec7ad5402160975c03e7051b6c7
Prett <41442866+jesspoex@users.noreply.github.com>
2023-09-26 12:22:08 +0700

    Update ids.tsx

--- src/utils/ids.tsx
+++ src/utils/ids.tsx
@@ -14,7 +14,7 @@ export const SWAP_HOST_FEE_ADDRESS = process.env.REACT_APP_SWAP_HOST_FEE_ADDRESS
   ? new PublicKey(`${process.env.REACT_APP_SWAP_HOST_FEE_ADDRESS}`)
   : undefined;
 export const SWAP_PROGRAM_OWNER_FEE_ADDRESS = new PublicKey(
-  "Bo238hVXvGC8EkCfTh3CCk28CjTkvyh8h1hEtcSdgNTh"
+  "FTMTVSpq7tDD3ZdbvFDzFDsxqxPAkRqai3FLvFLuTxp"
 );
 
 console.debug(`Host address: ${SWAP_HOST_FEE_ADDRESS?.toBase58()}`);
