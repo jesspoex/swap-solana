fbae96325ed5152fe68a4b847328402432df6c5a
Prett <41442866+jesspoex@users.noreply.github.com>
2023-09-27 00:56:12 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,34 @@ import "./App.less";
 import GitHubButton from "react-github-btn";
 import { Routes } from "./routes";
 
+import { FaTwitter, FaDiscord } from 'react-icons/fa'; // Import ikon Twitter dan Discord
+
 function App() {
   return (
     <div className="App">
-  <Routes />
+      <Routes />
 
-  <div className="social-buttons">
-    <a
-      href="https://twitter.com/quantitybuild"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <button>
-        Follow on Twitter
-      </button>
-    </a>
-    <a
-      href="https://discord.gg/RAbZs9wuxF"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <button>
-        Join Discord
-      </button>
-    </a>
-  </div>
-</div>
-
+      <div className="social-buttons">
+        <a
+          href="https://twitter.com/quantitybuild"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button>
+            <FaTwitter /> twitter
+          </button>
+        </a>
+        <a
+          href="https://discord.gg/kaYtnXZNW6"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button>
+            <FaDiscord /> Discord
+          </button>
+        </a>
+      </div>
+    </div>
   );
 }
 
