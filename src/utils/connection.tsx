256bb1110c9466330431b22f43facbbccc34e14d
Prett <41442866+jesspoex@users.noreply.github.com>
2023-09-28 11:07:59 +0700

    Update connection.tsx

--- src/utils/connection.tsx
+++ src/utils/connection.tsx
@@ -15,7 +15,7 @@ export type ENV = "mainnet-beta" | "testnet" | "devnet" | "localnet";
 export const ENDPOINTS = [
   {
     name: "mainnet-beta" as ENV,
-    endpoint: "https://price.jup.ag/v4",
+    endpoint: "https://api.mainnet-beta.solana.com",
   },
   { name: "testnet" as ENV, endpoint: clusterApiUrl("testnet") },
   { name: "devnet" as ENV, endpoint: clusterApiUrl("devnet") },
